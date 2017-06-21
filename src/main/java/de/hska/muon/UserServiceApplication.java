dc20006c4d0892ffc703a23912c2ad5f239c2d1d
gerrit <gerrit.leuchner@gmail.com>
2017-06-21 23:01:52 +0200

    oauth

--- src/main/java/de/hska/muon/UserServiceApplication.java
+++ src/main/java/de/hska/muon/UserServiceApplication.java
@@ -3,13 +3,29 @@ package de.hska.muon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableResourceServer
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Primary
+    @Bean
+    public RemoteTokenServices tokenService() {
+        RemoteTokenServices tokenService = new RemoteTokenServices();
+        tokenService.setCheckTokenEndpointUrl(
+                "http://authserver:8763/oauth/check_token");
+        tokenService.setClientId("my-client-with-secret");
+        tokenService.setClientSecret("secret");
+        return tokenService;
     }
 }
 
